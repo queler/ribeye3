d6f536d89a63b4f710dac00b832771a9a0cfbe5f
joelghill <joel.hill.87@gmail.com>
2013-12-11 15:16:25 -0600

    Added test functions for Batter class. Mostly just testing out Python and Git.

--- Batter.py
+++ Batter.py
@@ -6,7 +6,7 @@
 
 
 class Batter(object):
-    def __init__(self,offset,lineup_pos,name,stance,batting_avg,home_runs,contact,power1,power2,speed,position,switch):
+    def __init__(self, offset, lineup_pos, name, stance, batting_avg, home_runs, contact, power1, power2, speed, position , switch):
         self.offset = offset
         self.lineup_pos = lineup_pos
         self.name = name
@@ -16,26 +16,52 @@ class Batter(object):
         self.contact = contact
         self.power1 = power1
         self.power2 = power2
-        self.power = power1 +(power2*256)
+        self.power = power1 + (power2*256)
         self.speed = speed
         self.position = position
         self.switch = switch
 
     # a string representation of a batter
     def __str__(self):
-        return(str(hex(self.offset//2)) + '\t'+
-            str(self.lineup_pos) + '\t'+
-            self.name + '\t'+
-            str(self.stance) + '\t'+
-            str(self.batting_avg) + '\t'+
-            str(self.home_runs) + '\t'+
-            str(self.contact) + '\t'+
-            str(self.power) + '\t'+
-            str(self.speed) + '\t'+
-            str(self.position) + '\t'+
-            str(self.switch) + '\n')
+        return(str(hex(self.offset//2)) + '\t' +
+               str(self.lineup_pos) + '\t' +
+               self.name + '\t' +
+               str(self.stance) + '\t' +
+               str(self.batting_avg) + '\t' +
+               str(self.home_runs) + '\t' +
+               str(self.contact) + '\t' +
+               str(self.power) + '\t' +
+               str(self.speed) + '\t' +
+               str(self.position) + '\t' +
+               str(self.switch) + '\n')
 
 
+def testbatter():
+    """
+    test function. Creates instance of batter, prints to screen
+    @return: NULL
+    """
+    batter = Batter(8, 8, "Bill", 8, 8, 8, 8, 8, 8, 8, 8, 8)
+    assert batter.offset == 8
+    assert batter.lineup_pos == 8
+    assert batter.name == "Bill"
+    assert batter.stance == 8
+    assert batter.batting_avg == 8
+    assert batter.home_runs == 8
+    assert batter.contact == 8
+    assert batter.power1 == 8
+    assert batter.power2 == 8
+    assert batter.power == 8 + (8*256)
+    assert batter.speed == 8
+    assert batter.position == 8
+    assert batter.switch == 8
+    return
 
 
+def test__str__():
+    batter = Batter(8, 8, "Bill", 8, 8, 8, 8, 8, 8, 8, 8, 8)
+    print(batter.__str__())
+    return
 
+testbatter()
+test__str__()
