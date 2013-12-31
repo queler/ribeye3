c76db2073107c9283e53fe6cf2bfe9336c30664e
Brahm <neufeld@gmail.com>
2013-12-31 17:12:25 -0600

    Nothing is changed here but there is some useful debug code for building pitcher objects.

--- Ribeye3.py
+++ Ribeye3.py
@@ -8,30 +8,30 @@
 from PlayerEditor import *
 from Values import *
 
-game_file = "rbi2013-v4-WIP.nes"  # changed to .nes file from .txt file
+game_file = "rbi3.nes"  # changed to .nes file from .txt file
 
 def main():
     editor = PlayerEditor(game_file)
 
     # create a new instance of a pitcher - spaces added to make the padding work.
-    test_pitcher = Pitcher(PITCHER_S1,14,'Brahm   ',7,1,3.21,155,155,155,5,3,65,115,115)
+    #test_pitcher = Pitcher(PITCHER_S1,14,'Brahm   ',7,1,3.21,155,155,155,5,3,65,115,115)
 
     # this SHOULD insert my test pitcher into the editor object.
-    editor.players.pitchers[PITCHER_S1] = test_pitcher
+    #editor.players.pitchers[PITCHER_S1] = test_pitcher
 
     # print a test string to show the test_pitcher variable
-    print(str(test_pitcher))
+    #print(str(test_pitcher))
 
     # print to show that the test_pitcher is inside of the editor object.
-    print(str(editor.players.pitchers[PITCHER_S1]))
+    #print(str(editor.players.pitchers[PITCHER_S1]))
 
     # print the hex representation of the test_pitcher
-    print(PlayerEditHelper().pitcher_convert(test_pitcher))
+    #print(PlayerEditHelper().pitcher_convert(test_pitcher))
 
     # print the whole gamefile
     print(str(editor))
     # write the gamefile
-    #editor.write_file()
+    editor.write_file()
 
 if __name__ == "__main__":
     main()
